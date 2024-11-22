9d024e370875af1efe687179c574fc8d8040b3e0
drodr211 <you@example.com>
2024-11-22 07:48:57 -0800

    some message outputs and placeholder functions

--- main.py
+++ main.py
@@ -2,7 +2,27 @@ import random
 print("Hello world")
 
 # placenholder evalution function
-def evalFunc():
+def evalFSet():
     return random.randint(0, 100) 
 
-print(evalFunc())
+def randPercent():
+    return random.randint(0,1000) / 10
+
+def expandChildren():
+    return 0
+
+def forwardSearch():
+    return 0
+
+
+
+
+print("\nWelcome to drodr211's Feature Selection Algorithm.\n\n")
+
+numFeatures = int(input("Enter total number of features: \n\n  >>> ")) 
+
+algo = int(input("\nWhich algorithm to use?. \n\n    1. Forward Selection\n    2. Backwards Elimination\n\n  >>> "))
+
+print("Beginning Search")
+
+
