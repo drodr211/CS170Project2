ec336fe6608d0d325f2b42948e04354bf5f3edc0
drodr211 <drodr211@ucr.edu>
2024-11-22 14:02:25 -0800

    working forward search algorithm with output

--- helper.py
+++ helper.py
@@ -1,5 +1,5 @@
 from random import randint as rand
-
+import copy
 allFeatures = set()
 
 class FSet():
@@ -7,27 +7,66 @@ class FSet():
         self.set = set
         self.eval = evalFSet(self.set)
     def __repr__(self):
-        return f"Feature set {self.set} with accuracy {self.eval}%"
+        if self.set:
+            return f"Feature set {self.set} with accuracy {self.eval}%"
+        else:
+            return f"Empty feature set with accuracy {self.eval}%"
     
 def evalFSet(fset): return rand(0,1000) / 10 
 
 def expandFset(fset):
+    print("Expanding " , end="")
+    print(fset, end="")
+    print(". . .\n")
+    
+    maxChild = fset
     for i in allFeatures:
-        print(i)
         if i not in fset.set:
-            print(FSet(fset.set|{i}))
+            temp = copy.deepcopy(fset.set)
+            temp.add(i)
+            newChild = FSet(temp)
+            print("    ", end = "")
+            print(newChild)
+            if newChild.eval > maxChild.eval:
+                maxChild = copy.deepcopy(newChild)
+    print("\n")
+    return maxChild
+
+
+def reduceFset(fset):
+    print("Reducing " , end="")
+    print(fset, end="")
+    print(". . .\n")
+
+    maxChild = fset
+
+
 
     return 0
+
 
 def forwardSearch(numFeatures):
     global allFeatures
     allFeatures = set(range(1, numFeatures+1))
-    print(allFeatures)
-    expandFset(FSet(set()))
+    currSet = FSet(set())
+    print(f"\nStarting forward Search with empty set and random eval.\n")
+
+    while True:
+        newSet = expandFset(currSet)
+        if newSet == currSet:
+            print(f"=========== SEARCH COMPLETE =========== \n The best is {newSet}\n\n")
+            break
+        else:
+            currSet = newSet
+            continue
     return 0
 
 def backwardsElim(numFeatures):
-     return 0
+    global allFeatures
+    allFeatures = set(range(1, numFeatures+1))
+
+
+    return 0
 
 
 
--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 from helper import *
 
-
-
-
-
-
-
 print("\nWelcome to drodr211's Feature Selection Algorithm.\n\n")
 
 numFeatures = int(input("Enter total number of features: \n\n  >>> ")) 
-
 algo = int(input("\nWhich algorithm to use?. \n\n    1. Forward Selection\n    2. Backwards Elimination\n\n  >>> "))
-
-print("Beginning Search")
-
 
 match algo:
     case 1:
         forwardSearch(numFeatures)
     case 2:
         backwardsElim(numFeatures)
-
-
