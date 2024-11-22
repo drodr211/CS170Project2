1b86a78967c562d067daca6b5cb2184c37a413bc
drodr211 <drodr211@ucr.edu>
2024-11-22 14:31:15 -0800

    working backwards elim, formatting changes

--- helper.py
+++ helper.py
@@ -1,5 +1,5 @@
 from random import randint as rand
-import copy
+from copy import deepcopy as dcp
 allFeatures = set()
 
 class FSet():
@@ -7,65 +7,92 @@ class FSet():
         self.set = set
         self.eval = evalFSet(self.set)
     def __repr__(self):
-        if self.set:
-            return f"Feature set {self.set} with accuracy {self.eval}%"
-        else:
-            return f"Empty feature set with accuracy {self.eval}%"
+        if self.set:    return f"Feature set {self.set} with accuracy {self.eval}%"
+        else:           return f"Empty feature set with accuracy {self.eval}%"
     
 def evalFSet(fset): return rand(0,1000) / 10 
 
+
+def modifySet(set,i,mod):
+    temp = dcp(set)
+    if mod: temp.add(i)
+    else:   temp.remove(i)
+    return FSet(temp)
+
+
 def expandFset(fset):
-    print("Expanding " , end="")
+    print("Expanding ", end="")
     print(fset, end="")
     print(". . .\n")
     
     maxChild = fset
     for i in allFeatures:
         if i not in fset.set:
-            temp = copy.deepcopy(fset.set)
-            temp.add(i)
-            newChild = FSet(temp)
+            newChild = modifySet(fset.set, i, 1)
             print("    ", end = "")
             print(newChild)
             if newChild.eval > maxChild.eval:
-                maxChild = copy.deepcopy(newChild)
+                maxChild = dcp(newChild)
     print("\n")
     return maxChild
 
 
-def reduceFset(fset):
-    print("Reducing " , end="")
+def elimFset(fset):
+    print("Eliminating ", end="")
     print(fset, end="")
     print(". . .\n")
 
     maxChild = fset
-
-
-
-    return 0
+    for i in allFeatures:
+        if i in fset.set:
+            newChild = modifySet(fset.set, i, 0)
+            print("    ", end = "")
+            print(newChild)
+            if newChild.eval > maxChild.eval:
+                maxChild = dcp(newChild)
+    print("\n")
+    return maxChild
 
 
 def forwardSearch(numFeatures):
     global allFeatures
     allFeatures = set(range(1, numFeatures+1))
     currSet = FSet(set())
-    print(f"\nStarting forward Search with empty set and random eval.\n")
+    print(f"\nStarting Forward Search w/ empty set and random eval.\n")
 
+    iters = 0
     while True:
         newSet = expandFset(currSet)
+        iters += 1
         if newSet == currSet:
-            print(f"=========== SEARCH COMPLETE =========== \n The best is {newSet}\n\n")
+            print(f"=========== SEARCH COMPLETE =========== \n The best is {newSet}\n")
             break
         else:
             currSet = newSet
             continue
+    print(f"Finished in {iters} iterations.\n")
     return 0
 
 def backwardsElim(numFeatures):
     global allFeatures
     allFeatures = set(range(1, numFeatures+1))
 
+    currSet = FSet(allFeatures)
 
+    print(f"\nStarting Backwards Elim. w/ full set and random eval.\n")
+    
+    iters = 0
+    while True:
+        newSet = elimFset(currSet)
+        iters += 1
+        if newSet == currSet:
+            print(f"=========== SEARCH COMPLETE =========== \n The best is {newSet}\n")
+            break
+        else:
+            currSet = newSet
+            continue
+
+    print(f"Finished in {iters} iterations.\n")
     return 0
 
 
