861942d4665ac80f1051f3781cdc7d721b10c838
drodr211 <drodr211@ucr.edu>
2024-12-03 14:44:22 -0800

    create auxliary for mostly untouched functions

--- helper.py
+++ helper.py
@@ -1,4 +1,4 @@
-from random import randint as rand
+from auxiliary import *
 from copy import deepcopy as dcp
 import math
 
@@ -42,11 +42,6 @@ class classifier():
         self.convertToDecimal()                                         # convert string data to decimal
         self.normalize()                                                # normalize data (from 0 to 1)
     
-
-def evalFSet(fset): return rand(0,1000) / 10 
-
-def printClean(str) : print(str, end="")
-
 def modifySet(set,i,mod):
     temp = dcp(set)
     if mod: temp.add(i)         # add feature to set
@@ -123,30 +118,3 @@ def backwardsElim(numFeatures):
 
     print(f"Finished in {iters} iterations.\n")
     return 0
-
-def readIEEE(str):
-    number = float(str.split("e")[0])           # extracts number from IEEE
-    sign = str.split("e")[1][0]                 # extracts sign from IEEE 
-    exponent = float(str.split("e")[1][1:])     # extracts exponent from IEEE
-    
-    match sign:
-        case "+":
-            return round((number * (10 ** exponent)), 7)    # calc as negative exponent if sign is -
-        case "-":
-            return round((number * (10 ** -exponent)), 7)   # calc as positive exponent if sign is +
-        
-def identifyNumInstances(file):
-    f = open(file, "r")     # open file
-    cnt = 0                 # set count to 0
-    for x in f:             # for line in file
-        cnt += 1                # increment counter
-    f.close()               # close file
-    return cnt              # return count of lines in file
-
-def identifyNumFeatures(file):
-    f = open(file, "r")                                         # open file
-    line1 = f.readline()                                        # read one line
-    featuresList =  [i for i in line1.split(" ") if i != ""]    # splits string into all columns and removes spaces
-    featuresList = featuresList[1:]                             # removes the class label
-    f.close()                                                   # close file
-    return len(featuresList)                                    # return number of features
