7d5bee4487535137b470c874e3beb858c9d5a52c
drodr211 <drodr211@ucr.edu>
2024-12-03 14:40:27 -0800

    commenting

--- helper.py
+++ helper.py
@@ -14,31 +14,33 @@ class FSet():
 
 class classifier():
     def convertToDecimal(self):
-        for row in range(len(self.instances)):
-            for col in range(len(self.instances[row])):
-                self.instances[row][col] = readIEEE(self.instances[row][col])
+        for row in range(len(self.instances)):                                  # for every row
+            for col in range(len(self.instances[row])):                         # for every col 
+                self.instances[row][col] = readIEEE(self.instances[row][col])   # convert string data to decimal
+
     def normalize(self):
-        for col in range(1, self.numFeatures):
-            max = self.instances[1][col]
-            min = self.instances[1][col]
-            for row in range(len(self.instances)):
-                data = self.instances[row][col]
-                if data > max: max = data
-                if data < min: min = data
-            for row in range(len(self.instances)):
-                self.instances[row][col] = round((self.instances[row][col]-min)/(max-min), 7)   
+        for col in range(1, self.numFeatures):              # for every column in the matrix
+            max = self.instances[1][col]                    # initialize max
+            min = self.instances[1][col]                    # initialize min
+            for row in range(len(self.instances)):          # for every row for that column
+                data = self.instances[row][col]             # get data at row, col
+                if data > max: max = data                   # adjust max
+                if data < min: min = data                   # adjust min
+            for row in range(len(self.instances)):          # adjust all data in the column
+                self.instances[row][col] = round((self.instances[row][col]-min)/(max-min), 7) # normalize data (from 0 to 1)
+       
     def test(self, instance):
         return 0 #label
     
     def __init__(self, filename):
-        self.numFeatures = identifyNumFeatures(filename)
-        self.instances = []
-        f = open(filename)
-        for x in f:
-            self.instances.append([i for i in x.split(" ") if i != ""])
-        f.close()
-        self.convertToDecimal()
-        self.normalize()
+        self.numFeatures = identifyNumFeatures(filename)                # calc number of features (num of columns)
+        self.instances = []                                             # initialize empty list 
+        f = open(filename)                                              # open file
+        for x in f:                                                     # for line in file
+            self.instances.append([i for i in x.split(" ") if i != ""]) # split line to indiv feature vals and remove spaces
+        f.close()                                                       # close file
+        self.convertToDecimal()                                         # convert string data to decimal
+        self.normalize()                                                # normalize data (from 0 to 1)
     
 
 def evalFSet(fset): return rand(0,1000) / 10 
@@ -47,8 +49,8 @@ def printClean(str) : print(str, end="")
 
 def modifySet(set,i,mod):
     temp = dcp(set)
-    if mod: temp.add(i)
-    else:   temp.remove(i)
+    if mod: temp.add(i)         # add feature to set
+    else:   temp.remove(i)      # remove feature from set
     return FSet(temp)
 
 def expandFset(fset):
@@ -129,23 +131,22 @@ def readIEEE(str):
     
     match sign:
         case "+":
-            return round((number * (10 ** exponent)), 7)
+            return round((number * (10 ** exponent)), 7)    # calc as negative exponent if sign is -
         case "-":
-            return round((number * (10 ** -exponent)), 7)
+            return round((number * (10 ** -exponent)), 7)   # calc as positive exponent if sign is +
         
 def identifyNumInstances(file):
-    f = open(file, "r")
-    cnt = 0
-    for x in f:
-        cnt += 1
-    f.close()
-    return cnt
+    f = open(file, "r")     # open file
+    cnt = 0                 # set count to 0
+    for x in f:             # for line in file
+        cnt += 1                # increment counter
+    f.close()               # close file
+    return cnt              # return count of lines in file
 
 def identifyNumFeatures(file):
-    f = open(file, "r")
-    line1 = f.readline()            # read one line
-    featuresList =  [i for i in line1.split(" ") if i != ""]  # splits string into all columns and removes spaces
-    featuresList = featuresList[1:]          # removes the class id
-    f.close()
-
-    return len(featuresList)
+    f = open(file, "r")                                         # open file
+    line1 = f.readline()                                        # read one line
+    featuresList =  [i for i in line1.split(" ") if i != ""]    # splits string into all columns and removes spaces
+    featuresList = featuresList[1:]                             # removes the class label
+    f.close()                                                   # close file
+    return len(featuresList)                                    # return number of features
