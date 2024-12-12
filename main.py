848193bc20293b783285e14275ece67081062958
drodr211 <drodr211@ucr.edu>
2024-12-12 02:10:26 -0800

    results included in code

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import helper as h
 import os.path as p
 from helper import *
 
+# Results ###########################################################################
+#   Group: Daniel Rodriguez, drodr211, Lec001
+#       - Small Dataset Results:
+#           Forward: Feature Subset: {NULL_SET}, Acc: 75.0%
+#           Backward: Feature Subset: {3, 5}, Acc: 92.0%
+#       - Large Dataset Results:
+#           Forward: Feature Subset: {1,27}, Acc: 95.5%
+#           Backward: Feature Subset: {2-7, 9-20, 22-25, 27-40}, Acc: 73.1%
+#       - Cleaned Titanic Dataset Results:
+#           Forward: Feature Subset: {2}, Acc: 78.01%
+#           Backward: Feature Subset: {1, 2, 3, 6}, Acc: 75.21%
+#####################################################################################
+
 print("\nWelcome to drodr211's Feature Selection Algorithm.\n")
 
 h.fileName = input("Enter file name:\n\n  >>> ")
